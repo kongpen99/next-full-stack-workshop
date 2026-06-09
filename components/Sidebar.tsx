2dff11c1eb1e9220277dc824f1b1f6380c10a11a
kongpeng99 <k.kongpeng99@gmail.com>
2026-06-09 17:02:25 +0700

    api

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,23 +4,23 @@ import Link from 'next/link';
 
 export default function Sidebar() {
     return (
-        <div className="w-84 h-screen bg-gray-800 text-white p-4">
+        <div className="w-84 h-screen bg-gray-700 text-white p-4">
             <div className="mb-4 text-center bg-gray-900">
-                <div className="text-2xl font-bold">kApartment 1.0</div>
+                <div className="text-2xl font-bold">K&T Apartment 1.0</div>
                 <div className="text-xl mt-2">โปรแกรมบริหารจัดการอหอพักพาร์ตเมนต์</div>
             </div>
             <nav className="p-5"></nav>
 
             <ul className="sidebar-menu">
                 <li>
-                    <Link href="/home/apartment" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100">
+                    <Link href="/home/apartment" className="flex items-center p-2 text-base font-normal text-gray-300 rounded-lg hover:bg-gray-100">
                         <i className="fas fa-home"></i>
                         <span className="ml-3">ข้อมูลอพาร์ตเมนต์</span>
                     </Link>
                 </li>
                 <li>
-                    <Link href="/home/room-type" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100">
-                        <i className="fas fa-home"></i>
+                    <Link href="/home/room-type" className="flex items-center p-2 text-base font-normal text-gray-300 rounded-lg hover:bg-gray-100">
+                        <i className="fas solid fa-bed"></i>
                         <span className="ml-3">ประเภทห้องพัก</span>
                     </Link>
                 </li>
