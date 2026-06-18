6f6ac0021d95a671f5e7e968e644962f44be624a
kongpeng99 <k.kongpeng99@gmail.com>
2026-06-18 09:42:21 +0700

    เพิ่ม Seraui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 
+import Button from "@/components/button";
 export default function Home() {
   return (
     <div>
 
-
+      <Button variant="secondary">
+        Test Button
+      </Button>
     </div>
 
   );
