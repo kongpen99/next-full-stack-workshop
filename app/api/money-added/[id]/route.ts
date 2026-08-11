2d725303fe25cb123337a6da1905e2a41dd03dfc
kongpeng99 <k.kongpeng99@gmail.com>
2026-08-11 13:47:23 +0700

    fixbug ui

--- app/api/money-added/[id]/route.ts
+++ app/api/money-added/[id]/route.ts
@@ -1,18 +1,19 @@
 // PUT api/money-added/:id //อัพเดทข้อมูลการเพิ่มเงิน
 
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/libs/prisma";
 import { z } from "zod";
 
-export async function PUT (Request: Request, { params }: { params: { id: string } }) {
-    try{
+export async function PUT(Request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    try {
         const body = await Request.json();
         const schema = z.object({
             name: z.string(),
             amount: z.number()
         });
-        const {name ,amount} = schema.parse(body);
-        const {id} = await params;
+        const { name, amount } = schema.parse(body);
+        const { id } = await params;
         const moneyAdded = await prisma.moneyAdded.update({
             where: {
                 id: id
@@ -23,20 +24,20 @@ export async function PUT (Request: Request, { params }: { params: { id: string 
             }
         });
         return NextResponse.json(moneyAdded);
-        
+
     } catch (error) {
         return NextResponse.json(
-            { error: (error as Error).message }, 
+            { error: (error as Error).message },
             { status: 500 }
-        );        
+        );
     }
 }
 
 // DELETE api/money-added/:id //ลบข้อมูลการเพิ่มเงิน
 
-export async function DELETE (request: Request, { params }: { params: Promise<{ id: string }> }) {
-    try{
-        const {id} = await params;
+export async function DELETE(request: Request, { params }: { params: Promise<{ id: string }> }) {
+    try {
+        const { id } = await params;
         const moneyAdded = await prisma.moneyAdded.update({
             where: {
                 id: id
@@ -46,12 +47,12 @@ export async function DELETE (request: Request, { params }: { params: Promise<{ 
             }
         });
         return NextResponse.json(moneyAdded);
-        
+
     } catch (error) {
         return NextResponse.json(
-            { error: (error as Error).message }, 
+            { error: (error as Error).message },
             { status: 500 }
-        );        
+        );
     }
- 
+
 }
